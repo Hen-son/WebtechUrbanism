d4e126264192babfd798c62a81168b3599b40023
Henning Füller <henning.fuellerh@geo.hu-berlin.de>
2020-04-17 19:03:23 +0200

    kleinere Anpassungen

--- Erstelle_Karte.R
+++ Erstelle_Karte.R
@@ -1,18 +1,65 @@
-library(ggmap)
+library(RJSONIO)  #zum Konvertierung von JSON-Objekte
+library(ggplot2)  #zur Datenvisualisierung
+library(ggmap)    #zur Integration von Kartenmaterial bei der Datenvisualisierung in 
+library(dplyr)    #zur (komfortableren) Verarbeitung von Data-Frames
+library(knitr)    #zur Erstellung dieses Blogposts
+library(rmarkdown)#zur Erstellung dieses Blogposts
+library(nominatim)#Um Adressdaten zu suchen
 library(keyring)
-secret <- rstudioapi::askForSecret("Google API")
-register_google(key = secret, write = TRUE)
-library(tidyverse)
-library(readr)
-library(ggplot2)
 
-#Aktuelles Programm einlesen
+# get API Key from OS X Keychain
 
-HEXprogramm <- read.csv("data/HEX-programm.csv")
+secret <- rstudioapi::askForSecret("MapQuest Key")
 
-# Karte erzeugen
+secret.g <- rstudioapi::askForSecret("Google API Key")
 
-library(sf)
-library(mapview)
-HEX <- st_as_sf(HEXprogramm, coords = c("lon", "lat"), crs = 4326)
-mapview (HEX, zcol = "Ziele", legend = TRUE)
+
+#Arbeitsverzeichns festlegen
+setwd("/Users/fuellerh/Documents/Arbeit/Projekte/WebtechUrbanism")
+
+# Help function to look up addresses through OSM
+
+
+
+# Style of Map
+myMapTheme <- theme(legend.position="bottom",
+                    panel.grid.major=element_blank(),
+                    panel.grid.minor=element_blank(),
+                    axis.title=element_blank(),
+                    axis.ticks=element_blank(),
+                    axis.text=element_blank(),
+                    panel.background = element_rect(fill = "white"))
+
+
+#CSV-Sheet einlesen
+
+webtech_berlin <- read.csv("data/Webtech_Berlin.csv", stringsAsFactors=FALSE)
+
+# Using the function myOSMGeoCode to find coordinates
+
+for(i in 1:dim(webtech_berlin)[1]){
+  
+  street <- webtech_berlin[i, "Street"]
+  zip <- webtech_berlin[i, "ZIP"]
+  city <- webtech_berlin[i, "City"]
+  country <- webtech_berlin[i, "Country"]
+  
+  query <- c(street, zip, city, country)
+  
+  geo.data <- osm_geocode(query, key = secret)
+  
+  webtech_berlin[i,"Lon"] <- geo.data$lon[1]
+  webtech_berlin[i, "Lat"] <- geo.data$lat[1]
+  webtech_berlin[i, "Class"] <- geo.data$class[1]
+}
+
+# Get a Map of Berlin as a Background
+register_google (key=secret.g)
+map.berlin <- get_map(location = "Berlin, Germany", source = "google", color = "bw")
+
+wt_map <- ggmap(map.berlin)
+wt_map <- wt_map + geom_point(data=webtech_berlin, aes(x=Lon, y=Lat), color="blue", alpha=1, size=1)
+wt_map <- wt_map + geom_text(data=webtech_berlin, aes(x=Lon, y=Lat, label=Firma), hjust=0, vjust=0, fontface="plain", color="blue", size=2)
+g <- wt_map + myMapTheme
+g
+ggsave("WebtechKarteBerlin.png" )
