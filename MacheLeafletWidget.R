e7cf63fe567172e643b301c22a2c56f792454949
Henning Füller <henning.fuellerh@geo.hu-berlin.de>
2020-04-28 19:36:42 +0200

    Widget Production ergänzt

--- MacheLeafletWidget.R
+++ MacheLeafletWidget.R
@@ -1,23 +1,14 @@
----
-title: Webtech Urbanism
-output: html_document
----
-
-```{r setup, include=FALSE}
-knitr::opts_chunk$set(echo = TRUE)
 library(leaflet)
 library(dplyr)
 
-```{r, echo = FALSE}
 
 webtech_berlin <- read.csv("data/webtech_located.csv", stringsAsFactors=FALSE)
 label <- paste(sep = "<br/>", webtech_berlin$Firma, webtech_berlin$URL)
 
-leaflet(data = webtech_berlin) %>%
+WebtechKarte <- leaflet(data = webtech_berlin) %>%
   addTiles() %>% # Add default OpenStreetMap map tiles
   addMarkers(lng= ~Lon, lat= ~Lat, popup = ~as.character(label), label = ~as.character(Firma))
 
-```
-
-
-
+# # # save a stand-alone, interactive map as an html file
+library(htmlwidgets)
+saveWidget(widget = WebtechKarte, file = 'WebtechBerlin.html', selfcontained = T)
